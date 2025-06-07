d0084333a9b1d9544d9a1b88d4aa61579ebb2341
ODILJON-QOBILOV <qobilovodiljonoff@gmail.com>
2025-06-07 23:59:36 +0500

    port-env

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/anything/smth/1/config"
 	"github.com/anything/smth/1/controllers"
-	"github.com/gin-gonic/gin"
-	ginSwagger "github.com/swaggo/gin-swagger"
 	_ "github.com/anything/smth/1/docs"
-    "github.com/swaggo/files"
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // @title Url Shortener API
@@ -24,5 +26,10 @@ func main() {
 	r.GET("/:id", controllers.GetDataWithShortedUrl)
 	r.POST("/short-url", controllers.GetShortedUrl)
 
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port)
+
 }
